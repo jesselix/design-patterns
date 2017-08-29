089f3bb197e9d8898331dd6b1f9f90c6edc18970
Junying Li <jesselix@live.com>
2017-08-29 10:43:33 +0800

    =

---
+++ ZzDesignPatterns/src/main/java/li/jesse/behavioral/templatemethod/sample/Game.java
@@ -0,0 +1,25 @@
+package li.jesse.behavioral.templatemethod.sample;
+
+public abstract class Game
+{
+    private int playersCount;
+
+    abstract void initializeGame();
+
+    abstract void makePlay(int player);
+
+    abstract boolean endOfGame();
+
+    abstract void printWinner();
+
+    final void playOneGame(int playersCount) {
+        this.playersCount = playersCount;
+        initializeGame();
+        int j = 0;
+        while (!endOfGame()){
+            makePlay(j);
+            j = (j + 1) % playersCount;
+        }
+        printWinner();
+    }
+}
