50a2e436070c28afee63444bebf1fc7d3ddd39b0
Junying Li <jesselix@live.com>
2018-03-24 12:19:32 +0800

    =

---
+++ design-patterns-gof/src/main/java/li/jesse/structural/bridge/example/DrawingAPI.java
@@ -0,0 +1,6 @@
+package li.jesse.structural.bridge.example;
+
+public interface DrawingAPI
+{
+    public void drawCircle(double x, double y, double radius);
+}
--- design-patterns-gof/src/test/java/li/jesse/structural/bridge/BridgeTest.java
+++ design-patterns-gof/src/test/java/li/jesse/structural/bridge/BridgeTest.java
@@ -1,24 +1,22 @@
 package li.jesse.structural.bridge;
 
-//import li.jesse.structural.bridge.example.CircleShape;
-//import li.jesse.structural.bridge.example.DrawingAPI1;
-//import li.jesse.structural.bridge.example.DrawingAPI2;
-//import li.jesse.structural.bridge.example.Shape;
+import li.jesse.structural.bridge.example.CircleShape;
+import li.jesse.structural.bridge.example.DrawingAPI1;
+import li.jesse.structural.bridge.example.DrawingAPI2;
+import li.jesse.structural.bridge.example.Shape;
 import org.junit.Test;
 
 public class BridgeTest
 {
-//    @Test
-//    public void testBridge()
-//    {
-//        Shape[] shapes = new Shape[2];
-//        shapes[0] = new CircleShape(1, 2, 3, new DrawingAPI1());
-//        shapes[1] = new CircleShape(5, 7, 11, new DrawingAPI2());
-//
-//        for (Shape shape : shapes)
-//        {
-//            shape.resizeByPercentage(2.5);
-//            shape.draw();
-//        }
-//    }
+    @Test
+    public void testBridge() {
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new CircleShape(1, 2, 3, new DrawingAPI1());
+        shapes[1] = new CircleShape(5, 7, 11, new DrawingAPI2());
+
+        for (Shape shape : shapes) {
+            shape.resizeByPercentage(2.5);
+            shape.draw();
+        }
+    }
 }
